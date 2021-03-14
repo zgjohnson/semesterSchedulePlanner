f7d9384e36fa9891982eb6b33e012390ce212f30
Eagordon2021 <eagordon@ualr.edu>
2021-03-14 07:46:34 -0500

    Adding databases Course, Section and Reserved_time

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class course (models.Model):
+    departmentID = models.IntegerField
+    courseNumber = models.IntegerField
+    courseTitle = models.CharField(max_length=None)
+
+class section (models.Model):
+    departmentID = models.IntegerField
+    courseNumber = models.IntegerField
+
+class reservedTime (models.Model):
+    description = models.CharField(max_length=250)
+    startTime = models.TimeField(auto_now=False, auto_now_add=False)
+    endTime = models.TimeField(auto_now=False, auto_now_add=False)
+    sectionID = models.IntegerField
