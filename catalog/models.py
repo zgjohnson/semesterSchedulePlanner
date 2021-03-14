0ffc3ca77e2ea90feb39eca4ceb68b64ec679cb7
Eagordon2021 <eagordon@ualr.edu>
2021-03-14 07:55:11 -0500

    Added specific default values to CharFields Selected Primary Keys

--- catalog/models.py
+++ catalog/models.py
@@ -3,15 +3,15 @@ from django.db import models
 # Create your models here.
 class course (models.Model):
     departmentID = models.IntegerField
-    courseNumber = models.IntegerField
-    courseTitle = models.CharField(max_length=100, default"")
+    courseNumber = models.IntegerField(primary_key=True)
+    courseTitle = models.CharField(max_length=100, default="x")
 
 class section (models.Model):
-    departmentID = models.IntegerField
+    sectionID = models.IntegerField(primary_key=True)
     courseNumber = models.IntegerField
 
 class reservedTime (models.Model):
-    description = models.CharField(max_length=250, default"")
+    description = models.CharField(max_length=250, default="x")
     startTime = models.TimeField(auto_now=False, auto_now_add=False)
     endTime = models.TimeField(auto_now=False, auto_now_add=False)
-    sectionID = models.IntegerField
+    sectionID = models.IntegerField(primary_key=True)
