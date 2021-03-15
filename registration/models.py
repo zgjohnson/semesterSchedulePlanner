6c9ff35d14dd7d12d8c7a1b4747c103dc9095db2
Eagordon2021 <eagordon@ualr.edu>
2021-03-14 23:32:28 -0500

    Changing maxlength to max_length

--- registration/models.py
+++ registration/models.py
@@ -2,20 +2,20 @@ from django.db import models
 
 # Create your models here.
 class registrationRequest(models.Model):
-    proposedUsername = models.CharField(maxlength=20, null=False)
+    proposedUsername = models.CharField(max_length=20, null=False)
     proposedPassword = models.CharField(max_length=20, null=False)
     emailAddress = models.EmailField(max_length=254, null=False, primary_key=True)
     # Leaving out requestedAccessLevel and emailVerified, not sure how to approach yet
 
 class sspCredential (models.Model):
-    userName = models.CharField(maxlength=20, null=False)
-    password = models.CharField(maxlength=20, null=False)
+    userName = models.CharField(max_length=20, null=False)
+    password = models.CharField(max_length=20, null=False)
     emailAddress = models.EmailField(max_length=254, null=False, primary_key=True)
     # Leaving out accessLevel and emailVerified
 
 class student (models.Model):
     name = models.CharField(maxlength=250, null=False)
     emailAddress = models.EmailField(max_length=254, primary_key=True)
-    userName = models.CharField(maxlength=20, null=False)
-    password = models.CharField(maxlength=20, null=False)
+    userName = models.CharField(max_length=20, null=False)
+    password = models.CharField(max_length=20, null=False)
 
