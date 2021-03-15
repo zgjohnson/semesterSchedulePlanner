c08e084e52e47824cbb101b8338d1cf356768212
Eagordon2021 <eagordon@ualr.edu>
2021-03-14 23:23:57 -0500

    Moved reserved_time to the schedulegenerator model file Added registrationRequest, sspCredential and student to registration model file Added classPeriod to the catalog model file with composite primary key

--- catalog/models.py
+++ catalog/models.py
@@ -1,17 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
-class course (models.Model):
+class course(models.Model):
     departmentID = models.IntegerField
     courseNumber = models.IntegerField(primary_key=True, default=0)
-    courseTitle = models.CharField(max_length=100, default="x")
+    courseTitle = models.CharField(max_length=None, blank=True)
 
-class section (models.Model):
+
+class section(models.Model):
     sectionID = models.IntegerField(primary_key=True, default=0)
     courseNumber = models.IntegerField
 
-class reservedTime (models.Model):
-    description = models.CharField(max_length=250, default="x")
-    startTime = models.TimeField(auto_now=False, auto_now_add=False)
-    endTime = models.TimeField(auto_now=False, auto_now_add=False)
-    sectionID = models.IntegerField(primary_key=True, default=0)
+
+class classPeriod(models.Model):
+    class Meta:
+        unique_together = (('meetingDays', 'time'),)
+
+        meetingDays = models.CharField(primary_key=True, max_length=None)
+        time = models.TimeField()
+# used a solution from StackOverflow to make a composite key
+# https://stackoverflow.com/questions/28712848/composite-primary-key-in-django
--- scheduleGenerator/models.py
+++ scheduleGenerator/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class reservedTime (models.Model):
+    description = models.CharField(max_length=250, blank=True)
+    startTime = models.TimeField(auto_now=False, auto_now_add=False)
+    endTime = models.TimeField(auto_now=False, auto_now_add=False)
+    sectionID = models.IntegerField(primary_key=True, default=0)
