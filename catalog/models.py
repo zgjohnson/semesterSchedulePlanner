3beb4da4f13c6a3b08ee3c46707630055799ac43
Eagordon2021 <eagordon@ualr.edu>
2021-03-16 02:14:22 -0500

    Providing actual default value for department_ID and course_title

--- catalog/models.py
+++ catalog/models.py
@@ -4,9 +4,9 @@ from django.db import models
 # Create your models here.
 class course(models.Model):
 
-    department_ID = models.IntegerField(default=None)
+    department_ID = models.IntegerField(default="0")
    # courseNumber = models.IntegerField(primary_key=True, default=0, "Course Number")
-    course_title = models.CharField(max_length=250, blank=True)
+    course_title = models.CharField(max_length=250, default="blank")
 
 
 class section(models.Model):
