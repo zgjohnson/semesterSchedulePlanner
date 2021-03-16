b3571c3cb1998e95a578b266ac5c50cabb1d4631
Eagordon2021 <eagordon@ualr.edu>
2021-03-16 01:29:13 -0500

    Reinstating classPeriod model

--- registration/models.py
+++ registration/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 
 # Create your models here.
-class registrationRequest(models.Model):
-    proposedUsername = models.CharField(max_length=20, null=False)
-    proposedPassword = models.CharField(max_length=20, null=False)
-    emailAddress = models.EmailField(max_length=254, null=False, primary_key=True)
+# class registrationRequest(models.Model):
+  #  proposedUsername = models.CharField(max_length=20, null=False)
+   # proposedPassword = models.CharField(max_length=20, null=False)
+   # emailAddress = models.EmailField(max_length=254, null=False, primary_key=True)
     # Leaving out requestedAccessLevel and emailVerified, not sure how to approach yet
 
-class sspCredential (models.Model):
-    userName = models.CharField(max_length=20, null=False)
-    password = models.CharField(max_length=20, null=False)
-    emailAddress = models.EmailField(max_length=254, null=False, primary_key=True)
+# class sspCredential (models.Model):
+  #  userName = models.CharField(max_length=20, null=False)
+   # password = models.CharField(max_length=20, null=False)
+   # emailAddress = models.EmailField(max_length=254, null=False, primary_key=True)
     # Leaving out accessLevel and emailVerified
 
-class student (models.Model):
-    name = models.CharField(max_length=250, null=False)
-    emailAddress = models.EmailField(max_length=254, primary_key=True)
-    userName = models.CharField(max_length=20, null=False)
-    password = models.CharField(max_length=20, null=False)
+# class student (models.Model):
+  #  name = models.CharField(max_length=250, null=False)
+   # emailAddress = models.EmailField(max_length=254, primary_key=True)
+   # userName = models.CharField(max_length=20, null=False)
+   # password = models.CharField(max_length=20, null=False)
 
