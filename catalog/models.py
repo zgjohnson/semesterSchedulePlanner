5bd6de3b543f625d1198d6fdab323aa4e0225d74
Eagordon2021 <eagordon@ualr.edu>
2021-03-16 17:52:57 -0500

    Utilizing Django default ID system for primary key Converted classperiod to use a django ID instead of using composite key. All solutions to composite key were janky and it was reccomended to just add a singular primary attribute.

--- catalog/models.py
+++ catalog/models.py
@@ -3,21 +3,21 @@ from django.db import models
 
 # Create your models here.
 class course(models.Model):
-    departmentID = models.IntegerField
-    courseNumber = models.IntegerField(primary_key=True, default=0)
-    courseTitle = models.CharField(max_length=250, blank=True)
+
+    department_ID = models.IntegerField(default="")
+   # courseNumber = models.IntegerField(primary_key=True, default=0, "Course Number")
+    course_title = models.CharField(max_length=250, default="")
 
 
 class section(models.Model):
-    sectionID = models.IntegerField(primary_key=True, default=0)
-    courseNumber = models.IntegerField
+    # Using Django Default to provide ID key
+    # sectionID = models.IntegerField(primary_key=True, default=0, "Section ID", )
+    course_number = models.IntegerField(blank=True)
 
 
-#class classPeriod(models.Model):
- #   class Meta:
-  #      unique_together = (('meetingDays', 'time'),)
+class classPeriod(models.Model):
+    #Using Django Default to provide ID key
+    meeting_days = models.CharField(max_length=50, blank=True)
+    time = models.TimeField(blank=True)
 
-   #     meetingDays = models.CharField()
-    #    time = models.TimeField()
-# used a solution from StackOverflow to make a composite key
-# https://stackoverflow.com/questions/28712848/composite-primary-key-in-django
+
