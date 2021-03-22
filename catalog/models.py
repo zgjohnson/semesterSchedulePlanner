32acd690132ef28b1edeadb57771ed3e3e4fe613
Eagordon2021 <eagordon@ualr.edu>
2021-03-22 02:29:20 -0500

    Extended the max_length for Meeting Days, and removed the max_length for Instructor Name and Course Title

--- catalog/models.py
+++ catalog/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-
 # Create your models here.
+
 class Course(models.Model):
     #Using Course Number as primary key
     departmentID = models.CharField("Department ID", max_length=4)
     courseNumber = models.CharField("Course Number", max_length=4, primary_key=True)
-    courseTitle = models.CharField("Course Title", max_length=25)
+    courseTitle = models.CharField("Course Title", max_length=None)
 
     def __str__(self):
         return self.courseTitle
@@ -27,7 +27,7 @@ class Period(models.Model):
         ('TR', 'Tuesday, Thursday'),
         ('MTWRF', 'Monday, Tuesday, Wednesday, Thursday, Friday')
     )
-    meetingDays = models.CharField("Meeting Day", max_length=5, choices=DAY_CHOICES)
+    meetingDays = models.CharField("Meeting Day", max_length=10, choices=DAY_CHOICES)
 
     def __str__(self):
         return str(self.meetingDays) + " " + str(self.startTime) + "-" + str(self.endTime)
@@ -36,7 +36,7 @@ class Period(models.Model):
 class Section(models.Model):
     #Using SectionID as primary key
     sectionID = models.CharField("Section ID", max_length=4, primary_key=True)
-    instructor = models.CharField("Instructor", max_length=15)
+    instructor = models.CharField("Instructor", max_length=None)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     periods = models.ManyToManyField(Period)
 
