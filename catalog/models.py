02c56507fa1951a8d568f0e780ef04b9559c3b4c
Eagordon2021 <eagordon@ualr.edu>
2021-03-22 02:11:20 -0500

    Made custom primary keys for Course, readded comments to model section

--- catalog/models.py
+++ catalog/models.py
@@ -3,17 +3,19 @@ from django.db import models
 
 # Create your models here.
 class Course(models.Model):
-    departmentID = models.CharField("department ID", max_length=4)
-    courseNumber = models.CharField("course Number", max_length=4)
-    courseTitle = models.CharField("course Title", max_length=25, default='')
+    #Using Course Number as primary key
+    departmentID = models.CharField("Department ID", max_length=4)
+    courseNumber = models.CharField("Course Number", max_length=4, primary_key=True)
+    courseTitle = models.CharField("Course Title", max_length=25)
 
     def __str__(self):
         return self.courseTitle
 
 
 class Period(models.Model):
-    startTime = models.TimeField()
-    endTime = models.TimeField()
+    #Using Django default key
+    startTime = models.TimeField("Start Time")
+    endTime = models.TimeField("End Time")
     DAY_CHOICES = (
         ('M', 'Monday'),
         ('T', 'Tuesday'),
@@ -25,15 +27,16 @@ class Period(models.Model):
         ('TR', 'Tuesday, Thursday'),
         ('MTWRF', 'Monday, Tuesday, Wednesday, Thursday, Friday')
     )
-    meetingDays = models.CharField(max_length=5, choices=DAY_CHOICES)
+    meetingDays = models.CharField("Meeting Day", max_length=5, choices=DAY_CHOICES)
 
     def __str__(self):
         return str(self.meetingDays) + " " + str(self.startTime) + "-" + str(self.endTime)
 
 
 class Section(models.Model):
-    sectionID = models.CharField("section ID", max_length=4)
-    instructor = models.CharField(max_length=15)
+    #Using SectionID as primary key
+    sectionID = models.CharField("Section ID", max_length=4, primary_key=True)
+    instructor = models.CharField("Instructor", max_length=15)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     periods = models.ManyToManyField(Period)
 
