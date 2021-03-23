39f303004360c3e7a8cdfa6cad5bf0f0b0ae4b7e
zgjohnson <zgjohnson@ualr.edu>
2021-03-22 23:03:00 -0500

    registered accounts app instead of registration app so I can modify redux backend

--- semesterSchedulePlanner/settings.py
+++ semesterSchedulePlanner/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     #'django.contrib.sites',
-    'registration',
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalog',
+
 
 ]
 
